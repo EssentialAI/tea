41653942d31a165f7469b847a24cd315adc00bd5
NareshKumar Devulapally <naresh017edu@gmail.com>
2022-03-17 09:25:45 +0530

    binder opens in new tab

---
+++ _build/jupyter_execute/probability/probability-intro.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # [3.0] Probability Review
+
+# <span class = 'high'>Introduction to Probability</span>
+# 
+# The language of this course is going to be probability. Everything we discuss in this course will be based on undergraduate level probability. Probability is the study of uncertainty. The mathematical theory of probability is very sophisticated, and delves into a branch of analysis known as measure theory. In these notes, we provide a basic treatment of probability that does not address these finer details.
