b6bb25d57086de1bb375abb276a412c8ff1804db
NareshKumar Devulapally <naresh017edu@gmail.com>
2022-03-18 17:07:38 +0530

    started blog section

---
+++ _build/jupyter_execute/blog/blog-intro.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The Essential AI blog
+
+# In[2]:
+
+
+# Intro to blog
+
---
+++ _build/jupyter_execute/blog/mnist-4-neurons/MNIST-classification-with-4-output-neurons.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Can 4 output neurons classify 10 classes in MNIST?
+
+# In[2]:
+
+
+import pandas as pd
+print("Test")
+
