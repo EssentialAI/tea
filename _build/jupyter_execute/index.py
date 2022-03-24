31b774e8186313b2198dc8419a6ada465834e4d5
Naresh <naresh017edu@gmail.com>
2022-03-24 21:17:40 +0530

    added new files

---
+++ _build/jupyter_execute/index.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The Essential AI
+# 
+# **The Essential AI** aims to enable students from any educational background to pursue a career in Artificial Intelligence. We believe there is a disconnect between learning and understanding AI, and that math is the only way to bridge this gap. And this we try to achieve by instilling a sense of mathematical intuition that is necessary to understand AI.
+# 
+# <hr>
+# 
+# ## Why Math?
+# 
+# <h4><a target = "_blank" href = "https://qr.ae/pGLiWZ" class = 'nlinks'>How important is a strong mathematical background in machine learning?</a></h4>
+# 
+# <h4><a target = "_blank" href = "https://www.kdnuggets.com/2020/04/mathematics-machine-learning-book.html" class = 'nlinks'>Mathematics for Machine Learning by KDnuggets</a></h4>
+# 
+# <h4><a target = "_blank" href = "https://www.knowledgehut.com/blog/data-science/importance-of-maths-in-machine-learning" class = 'nlinks'>The Role of Mathematics in Machine Learning</a></h4>
+# 
+# <hr>
+# 
+# ## How to use this online book?
+# 
+# Jupyter notebooks allow users to present their work both as a *markdown* and in the form of an *executable code*. Jupyter-book combines both these features in the form of an online book that contains both static text and executable code that you can interact with. All of the content in this online book is written and presented in the form of Jupyter Notebook “chapters”.
+# 
+# ```{note}
+# You can run, and even modify, the example code in real time to get full benefits of Jupyter Notebooks!! This interaction allows you to experiment with different parameters and observe instantly how results change.
+# ```
+# 
+# <hr>
+# 
+# ## Interacting with Jupyter Notebooks in the cloud
+# 
+# ### Colab
+# 
+# The easiest way to navigate through these jupyter notebooks is through Google Colab. Just click the rocket ship icon <i class="fa fa-rocket" aria-hidden="true"></i> and select "Colab" to open the notebook in Google Colab. Connect to an instance and that's it! You have access to all the pre-installed libraries in Google Colab.
+# 
+# ### Binder
+# 
+# Another easy way to interact with a Jupyter Notebook is through
+# <a target="_blank" href = "https://mybinder.org/">Binder</a>, which enables the execution of a
+# jupyter notebook in the cloud. To execute these notebooks simply navigate your mouse to
+# the top right corner of the book chapter you are viewing and click
+# on the rocket ship icon <i class="fa fa-rocket" aria-hidden="true"></i>, and be sure to select
+# "Launch Binder". After a moment you should be presented with a
+# notebook that you can interact with, i.e. you will be able to execute
+# and even change the example programs. You will see that the code cells
+# have no output at first, until you execute them by pressing
+# {kbd}`Shift`\+{kbd}`Enter`.
